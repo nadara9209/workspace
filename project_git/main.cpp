417a83605104f211c62b79617c683523b3794386
SungGyu Park <nadara9209@gmail.com>
2018-11-18 14:57:46 +0900

    test

---
+++ project_git/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <algorithm>
+
+int main() {
+    
+    int N;
+    scanf("%d", &N);
+
+    int* arr = (int*)malloc(sizeof(int) * N);
+
+    for (int i = 0; i < N; ++i) {
+
+    }
+}
